02a40589acf333fa94bcd6159263800cae9f2754
DeepshikaS <ddeepu13@gmail.com>
2020-09-10 22:22:45 +0530

    random quote

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = ({ buttonDisplayName, clickHandler }) => {
+  return <button onClick={clickHandler}>{buttonDisplayName}</button>;
+};
+
+export default Button;
