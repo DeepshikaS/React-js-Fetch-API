06e566936dc93679ab6c968982b59b883811b748
DeepshikaS <ddeepu13@gmail.com>
2020-09-12 20:01:40 +0530

    quote generator

--- src/App.js
+++ src/App.js
@@ -1,79 +1,13 @@
 import React, { Component } from "react";
 //import Fr from "./components/Fetch_api";
 import "bootstrap/dist/css/bootstrap.min.css";
-import QuoteGenerator from "./components/QuoteGenerator";
-import { random } from "lodash";
+import Quotegen from "./components/Quotegen/Quotegen";
 import "./App.css";
-import "typeface-roboto";
-import { Grid, withStyles } from "@material-ui/core";
-
-const styles = {
-  container: {
-    display: "flex",
-    height: "100vh",
-    alignItems: "center",
-  },
-};
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      quotes: [],
-      selectedQuoteIndex: null,
-    };
-    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
-    this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
-  }
-
-  componentDidMount() {
-    fetch(
-      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
-    )
-      .then((data) => data.json())
-      .then((quotes) =>
-        this.setState({ quotes: quotes }, this.assignNewQuoteIndex)
-      );
-  }
-
-  get selectedQuote() {
-    if (
-      !this.state.quotes.length ||
-      !Number.isInteger(this.state.selectedQuoteIndex)
-    ) {
-      return undefined;
-    }
-    return this.state.quotes[this.state.selectedQuoteIndex];
-  }
-
-  selectQuoteIndex() {
-    if (!this.state.quotes.length) {
-      return undefined;
-    }
-    return random(0, this.state.quotes.length - 1);
-  }
-
-  assignNewQuoteIndex() {
-    this.setState({ selectedQuoteIndex: this.selectQuoteIndex() });
-  }
-
   render() {
-    return (
-      <Grid
-        className={this.props.classes.container}
-        id="quote-box"
-        justify="center"
-        container
-      >
-        <Grid xs={11} lg={8} item>
-          <QuoteGenerator
-            selectedQuote={this.selectedQuote}
-            assignNewQuoteIndex={this.assignNewQuoteIndex}
-          />
-        </Grid>
-      </Grid>
-    );
+    return <Quotegen />;
   }
 }
 
-export default withStyles(styles)(App);
+export default App;
---
+++ src/components/Quotegen/Quotegen.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+//import Fr from "./components/Fetch_api";
+import "bootstrap/dist/css/bootstrap.min.css";
+import QuoteGenerator from "./QuoteGenerator";
+import { random } from "lodash";
+import "../../App.css";
+import "typeface-roboto";
+import { Grid, withStyles } from "@material-ui/core";
+
+const styles = {
+  container: {
+    display: "flex",
+    height: "100vh",
+    alignItems: "center",
+  },
+};
+
+class Quotegen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null,
+    };
+    this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+    this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
+  }
+
+  componentDidMount() {
+    fetch(
+      "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json"
+    )
+      .then((data) => data.json())
+      .then((quotes) =>
+        this.setState({ quotes: quotes }, this.assignNewQuoteIndex)
+      );
+  }
+
+  get selectedQuote() {
+    if (
+      !this.state.quotes.length ||
+      !Number.isInteger(this.state.selectedQuoteIndex)
+    ) {
+      return undefined;
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex];
+  }
+
+  selectQuoteIndex() {
+    if (!this.state.quotes.length) {
+      return undefined;
+    }
+    return random(0, this.state.quotes.length - 1);
+  }
+
+  assignNewQuoteIndex() {
+    this.setState({ selectedQuoteIndex: this.selectQuoteIndex() });
+  }
+
+  render() {
+    return (
+      <Grid
+        className={this.props.classes.container}
+        id="quote-box"
+        justify="center"
+        container
+      >
+        <Grid xs={11} lg={8} item>
+          <QuoteGenerator
+            selectedQuote={this.selectedQuote}
+            assignNewQuoteIndex={this.assignNewQuoteIndex}
+          />
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withStyles(styles)(Quotegen);
